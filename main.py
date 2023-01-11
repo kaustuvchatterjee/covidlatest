e000fabb8bcdef87798e58a37a3230be26bf2ac1
kaustuvchatterjee <75204962+kaustuvchatterjee@users.noreply.github.com>
2023-01-11 09:38:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,14 +25,19 @@ for i in range(len(df)-1):
 fig, ax = plt.subplots(2,1, figsize=(12,10), sharex=True)
 
 ax[0].plot(df['obsDate'][1:],newcases, label='New Cases', color='r')
+ax[0].text(df['obsDate'][-1],newcases[-1], str(newcases)
 ax[0].plot(df['obsDate'][1:],newrec, label='New Recoveries', color='g')
+ax[0].text(df['obsDate'][-1],newrec[-1], str(newrec)
 ax[0].plot(df['obsDate'][1:],newdeaths, label='New Deathss', color='k')
+ax[0].text(df['obsDate'][-1],newdeaths[-1], str(newdeaths)
 ax[0].legend()
 ax[0].grid()
 ax[0].set_title('New Cases/Recoveries/Deaths')
 ax[0].set_ylim(bottom=0)
 
 ax[1].plot(df['obsDate'],df['active'], label='New Recoveries', color='r')
+ax[0].text(df['obsDate'][-1],df['active'], str(df['active'])
+
 ax[1].grid()
 ax[1].set_title('Active Cases')
 ax[1].set_ylabel('Count')
